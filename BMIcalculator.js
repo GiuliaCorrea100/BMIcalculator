a5133b252a7b4cc7877cedb11cd3e18b29b1ea13
Giulia Correa <124733742+GiuliaCorrea100@users.noreply.github.com>
2024-08-16 16:47:01 -0400

    Add files via upload

---
+++ BMIcalculator.js
@@ -0,0 +1,13 @@
+function bmiCalculator(weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+
+    if (bmi < 18.5) {
+        return "Your BMI is " + bmi + ", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return "Your BMI is " + bmi + ", so you have a normal weight.";
+    } else {
+        return "Your BMI is " + bmi + ", so you are overweight.";
+    }
+}
+var message = bmiCalculator(57, 1.5);
+console.log(message);
